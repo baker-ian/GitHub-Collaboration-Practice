2b168f1f15c5bdaf99b89209d813994d59cef961
Ian Baker <bakerian961@gmail.com>
2025-04-17 11:54:41 -0400

    Added Hello World to the Class Student004

--- GitHub Collaboration Practice/Student004.cs
+++ GitHub Collaboration Practice/Student004.cs
@@ -8,5 +8,9 @@ namespace GitHub_Collaboration_Practice
 {
     internal class Student004
     {
+        public static void HelloWorld()
+        {
+            Console.WriteLine("Hello World");
+        }
     }
 }
